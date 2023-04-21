47a7b4d82210665072311f5febfc2f6abbb1dc8c
Rohan Fredriksson <rohan.fredriksson@gmail.com>
2023-04-22 00:10:58 +1000

    Updated to reflect engine changes

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@ void TitleInit(Scene* scene) {
     SpriteRenderer* spriterenderer;
     Animation* animation;
     AnimationState* state;
+    FadeTransition* transition;
 
     // Background
     entity = new Entity(vec2(0.0f, 0.0f), vec2(5.0f, 5.0f), 0.0f);
@@ -21,6 +22,15 @@ void TitleInit(Scene* scene) {
     spriterenderer->setZIndex(-1);
     spriterenderer->setColour(vec4(0.5f, 0.0f, 0.5f, 1.0f));
     entity->addComponent(spriterenderer);
+    scene->addEntity(entity);
+
+    // Transition
+    entity = new Entity(vec2(0.0f, 0.0f), vec2(1.0f, 1.0f), 0.0f);
+    transition = new FadeTransition();
+    transition->setDuration(1.0f);
+    transition->setFrom(vec4(0.0f, 0.0f, 0.0f, 1.0f));
+    transition->setTo(vec4(0.0f, 0.0f, 0.0f, 0.0f));
+    entity->addComponent(transition);
     scene->addEntity(entity);
 
     // Character
